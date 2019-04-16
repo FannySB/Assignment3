11fe0dd9972867b797d3be7a3552d93d396e3297
Lawrence Abdulnour <Lawrenceee94@gmail.com>
2019-04-16 10:31:03 -0400

    updates on Q1.3

--- train.py
+++ train.py
@@ -10,12 +10,13 @@ import samplers as samplers
 import math
 import pdb
 
+
 class MLPNet(nn.Module):
     def __init__(self, input_dim=2):
         super(MLPNet, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 1)
+        self.fc1 = nn.Linear(input_dim, 10)
+        self.fc2 = nn.Linear(10, 20)
+        self.fc3 = nn.Linear(20, 1)
         self.input_dim = input_dim
 
     def forward(self, x):
@@ -25,7 +26,8 @@ class MLPNet(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        x = F.sigmoid(x)
+        #x = torch.log(torch.tensor(2).float()) - torch.log(1 + torch.exp(-x))
+        #x = F.sigmoid(x)
         return x
 
 
@@ -37,47 +39,43 @@ def estimatedJSD(epoch=10, batch_size=512, phi=0.9):
 
     for epoch in range(epoch):
 
-        x = samplers.distribution1(0, batch_size)
-        for input_x in x:
-            break
-        y = samplers.distribution1(phi, batch_size)
-        for input_y in y:
-            break
-
         optimizer.zero_grad()
 
-        input_x = Variable(torch.from_numpy(input_x)).float()
-        out_x = model(input_x)
+        x = samplers.distribution1(0, batch_size)
 
-        input_y = Variable(torch.from_numpy(input_y)).float()
-        out_y = model(input_y)
+        for input_x in x:
+            input_x = Variable(torch.from_numpy(input_x)).float()
+            out_x = model(input_x)
+            break
+
+        y = samplers.distribution1(phi, batch_size)
+
+        for input_y in y:
+            input_y = Variable(torch.from_numpy(input_y)).float()
+            out_y = model(input_y)
+            break
+
 
         sum_x = torch.sum(torch.log(out_x))
         sum_y = torch.sum(torch.log(1-out_y))
-
-        #for cpt in range(batch_size):
-            #sum_y = torch.log(1 - out_y[cpt])
-            #sum_x = torch.log(out_x[cpt])
 
         total_x = sum_x/ (2*batch_size)
         total_y = sum_y/ (2*batch_size)
 
         loss = - (total_x + total_y + torch.log(torch.tensor(2).float()))
-        #ave_loss += loss.data[0]
+
         loss.backward()
         optimizer.step()
 
-        print('epoch: {}, train loss: {:.6f}'.format(
-            epoch, loss))
+        if(epoch%50) == 0:
+            print('epoch: {}, train loss: {:.6f}'.format(
+                epoch, loss))
 
-    #compute the JSD
-    # out_opt_x = model()
     return model, loss
 
-def compute_js(D, batch_size, x, y):
+def compute_js(D, batch_size):
     sum_x= torch.sum(torch.log(D))
-    sum_y= torch.sum(torch.log(1.0-D))
-
+    sum_y= torch.sum(torch.log(1-D))
     return sum_x/(2*batch_size) + sum_y/(2*batch_size) + torch.log(torch.tensor(2).float())
 
 
@@ -85,18 +83,27 @@ batch_size = 512
 phi = -1.0
 js = []
 losses = []
+phi_ = []
 while not phi > 1.0:
     print(phi)
-    model, loss = estimatedJSD(batch_size=batch_size, epoch=100, phi = phi)
-    losses.append(loss.data[0])
-    x = samplers.distribution1(0, batch_size)
-    for input_x in x:
+    phi_.append(phi)
+    model, loss = estimatedJSD(batch_size=batch_size, epoch=500, phi=phi)
+    losses.append(-loss.data[0])
+
+    x_new = samplers.distribution1(0, batch_size)
+    for input_x_new in x_new:
+        D_star = model(Variable(torch.from_numpy(input_x_new)).float())
         break
-    y = samplers.distribution1(phi, batch_size)
-    for input_y in y:
-        break
-    D_star = model(Variable(torch.from_numpy(input_x)).float())
-    js.append(compute_js(D_star, batch_size, input_x, input_y).data[0])
-    print('js', js)
-    print('losses', losses)
-    phi += 0.5
+
+    js.append(compute_js(D_star, batch_size).data[0])
+    phi += 0.1
+
+
+print('js', js)
+print('losses', losses)
+plt.plot(phi_, js)
+plt.savefig('js.png')
+plt.clf()
+plt.plot(phi_, losses)
+plt.savefig('losses.png')
+print('==============End============')
