52892a178b90cb7bb07ee0f8e3f8468e1e762e57
Zigzagzen7 <alexandre.marcil@gmail.com>
2019-04-16 12:52:44 -0400

    VAE

--- vae.py
+++ vae.py
@@ -16,11 +16,18 @@ from torch import nn
 from torch.nn.modules import upsampling
 from torch.functional import F
 from torch.optim import Adam
+from torch.autograd import Variable
 
+import pdb
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+batch_size = 1
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
+# parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+#                     help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
@@ -40,10 +47,10 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
                    transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
+    batch_size=batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
+    batch_size=batch_size, shuffle=True, **kwargs)
 
 
 def get_data_loader(dataset_location, batch_size):
@@ -55,7 +62,7 @@ def get_data_loader(dataset_location, batch_size):
     for splitname in ["train", "valid", "test"]:
         filename = "binarized_mnist_%s.amat" % splitname
         filepath = os.path.join(dataset_location, filename)
-        utils.download_url(URL + filename, dataset_location)
+        utils.download_url(URL + filename, dataset_location, filename, md5=None)
         with open(filepath) as f:
             lines = f.readlines()
         x = lines_to_np_array(lines).astype('float32')
@@ -100,8 +107,221 @@ class VAE(nn.Module):
         return self.decode(z), mu, logvar
 
 
-model = VAE().to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+# class Interpolate(nn.Module):
+#     def __init__(self, size, mode):
+#         super(Interpolate, self).__init__()
+#         self.interp = nn.functional.interpolate
+#         self.size = size
+#         self.mode = mode
+#
+#     def forward(self, x):
+#         x = self.interp(x, size=self.size, mode=self.mode, align_corners=False)
+#         return x
+
+class VAE2(nn.Module):
+    def __init__(self):
+        super(VAE2, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=(3,3)),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=(3, 3)),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 256, kernel_size=(5, 5)),
+            nn.ELU(),
+            # nn.Linear(256, 2*100)
+        )
+        self.encoder_out1 = nn.Linear(256*128, 100)
+        self.encoder_out2 = nn.Linear(256*128, 100)
+
+
+        self.decoder = nn.Sequential(
+            nn.Linear(100, 256),
+            nn.ELU(),
+            nn.Conv2d(256, 64, kernel_size=(5, 5), padding=(4,4)),
+            nn.ELU(),
+            nn.UpsamplingBilinear2d(scale_factor=2),  # mode='bilinear'),
+            # Interpolate(scale_factor=2, mode='bilinear'),
+            nn.Conv2d(64, 32, kernel_size=(3, 3), padding=(2, 2)),
+            nn.ELU(),
+            nn.UpsamplingBilinear2d(scale_factor=2),  # mode='bilinear'),
+            nn.Conv2d(32, 16, kernel_size=(3, 3), padding=(2, 2)),
+            nn.ELU(),
+            nn.Conv2d(16, 1, kernel_size=(3, 3), padding=(2, 2)),
+            nn.Sigmoid() # ????
+        )
+
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        esp = torch.randn(*mu.size())
+        z = mu + std * esp
+        return z
+
+    def bottleneck(self, h):
+        mu = self.encoder_out1(h)
+        logvar = self.encoder_out2(h)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+    def encode(self, x):
+        h = self.encoder(x)
+        z, mu, logvar = self.bottleneck(h)
+        return z, mu, logvar
+
+    def decode(self, z):
+        z = self.decoder(z)
+        return z
+
+    def forward(self, x):
+        z, mu, logvar = self.encode(x)
+        z = self.decode(z)
+        return z, mu, logvar
+
+    # def encode(self, x):
+    #     # h1 = F.relu(self.fc1(x))
+    #     return self.encoder(x)
+    #
+    # def reparameterize(self, mu, logvar):
+    #     std = torch.exp(0.5*logvar)
+    #     eps = torch.randn_like(std)
+    #     return mu + eps*std
+    #
+    # def decode(self, z):
+    #     # h3 = F.relu(self.fc3(z))
+    #     return self.decoder(x)
+    #
+    # def forward(self, x):
+    #     mu, logvar = self.encode(x.view(-1, 784))
+    #     z = self.reparameterize(mu, logvar)
+    #     return self.decode(z), mu, logvar
+
+
+
+class VAE3(nn.Module):
+    def __init__(self):
+        super(VAE3, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(4, 4), padding=(15, 15),
+                               stride=2)  # This padding keeps the size of the image same, i.e. same padding
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(4, 4), padding=(15, 15), stride=2)
+        self.fc11 = nn.Linear(in_features=128 * 28 * 28, out_features=1024)
+        self.fc12 = nn.Linear(in_features=1024, out_features=100)
+
+        self.fc21 = nn.Linear(in_features=128 * 28 * 28, out_features=1024)
+        self.fc22 = nn.Linear(in_features=1024, out_features=100)
+        self.relu = nn.ReLU()
+
+        # For decoder
+
+        # For mu
+        self.fc1 = nn.Linear(in_features=100, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=7 * 7 * 128)
+        self.conv_t1 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, padding=1, stride=2)
+        self.conv_t2 = nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=4, padding=1, stride=2)
+
+
+
+    def encode(self, x: Variable) -> (Variable, Variable):
+
+        x = x.view(-1, 1, 28, 28)
+        x = F.elu(self.conv1(x))
+        x = F.elu(self.conv2(x))
+        x = x.view(-1, 128 * 28 * 28)
+
+        mu_z = F.elu(self.fc11(x))
+        mu_z = self.fc12(mu_z)
+
+        logvar_z = F.elu(self.fc21(x))
+        logvar_z = self.fc22(logvar_z)
+
+        return mu_z, logvar_z
+
+
+    def reparameterize(self, mu: Variable, logvar: Variable) -> Variable:
+
+        # def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        esp = torch.randn(*mu.size())
+        z = mu + std * esp
+        return z
+
+        # if self.training:
+        #     # multiply log variance with 0.5, then in-place exponent
+        #     # yielding the standard deviation
+        #
+        #     sample_z = []
+        #     for _ in range(10):
+        #         std = logvar.mul(0.5).exp_()  # type: Variable
+        #         eps = Variable(std.data.new(std.size()).normal_())
+        #         sample_z.append(eps.mul(std).add_(mu))
+        #
+        #     return sample_z
+        #
+        # else:
+        #     # During inference, we simply spit out the mean of the
+        #     # learned distribution for the current input.  We could
+        #     # use a random sample from the distribution, but mu of
+        #     # course has the highest probability.
+        #     return mu
+
+
+    def decode(self, z: Variable) -> Variable:
+
+        x = F.elu(self.fc1(z))
+        x = F.elu(self.fc2(x))
+        x = x.view(-1, 128, 7, 7)
+        x = F.relu(self.conv_t1(x))
+        x = F.sigmoid(self.conv_t2(x))
+
+        return x.view(-1, 784)
+
+
+    def forward(self, x: Variable) -> (Variable, Variable, Variable):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        # if self.training:
+        #     return [self.decode(z) for z in z], mu, logvar
+        # else:
+        return self.decode(z), mu, logvar
+        # return self.decode(z), mu, logvar
+
+    def loss_function(self, recon_x, x, mu, logvar) -> Variable:
+        # how well do input x and output recon_x agree?
+
+        # if self.training:
+        #     BCE = 0
+        #     for recon_x_one in recon_x:
+        #         BCE += F.binary_cross_entropy(recon_x_one, x.view(-1, 784))
+        #     BCE /= len(recon_x)
+        # else:
+        # pdb.set_trace()
+
+        BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784))
+
+        # KLD is Kullback–Leibler divergence -- how much does one learned
+        # distribution deviate from another, in this specific case the
+        # learned distribution from the unit Gaussian
+
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # - D_{KL} = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        # note the negative D_{KL} in appendix B of the paper
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        # Normalise by same number of elements as in reconstruction
+        KLD /= batch_size
+        # print ("BCE : ", BCE)
+        # print ("KLD : ", KLD)
+
+        return BCE + KLD
+
+
+model = VAE3().to(device)
+optimizer = optim.Adam(model.parameters(), lr=3e-4)
 
 
 # Reconstruction + KL divergence losses summed over all elements and batch
@@ -124,7 +344,7 @@ def train(epoch):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
+        loss = model.loss_function(recon_batch, data, mu, logvar)
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
@@ -149,7 +369,7 @@ def test(epoch):
             if i == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
-                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+                                      recon_batch.view(batch_size, 1, 28, 28)[:n]])
                 save_image(comparison.cpu(),
                          'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
@@ -158,21 +378,18 @@ def test(epoch):
 
 if __name__ == "__main__":
 
-    train, valid, test = get_data_loader("binarized_mnist", 64)
+    # train, valid, test = get_data_loader("binarized_mnist", 64)
 
-    import matplotlib
-    import matplotlib.pyplot as plt
+    # for x in train:
+    #     plt.imshow(x[0, 0])
+    #     break
 
-    for x in train:
-        plt.imshow(x[0, 0])
-        break
 
-    #
-    # for epoch in range(1, args.epochs + 1):
-    #     train(epoch)
-    #     test(epoch)
-    #     with torch.no_grad():
-    #         sample = torch.randn(64, 20).to(device)
-    #         sample = model.decode(sample).cpu()
-    #         save_image(sample.view(64, 1, 28, 28),
-    #                    'results/sample_' + str(epoch) + '.png')
+    for epoch in range(1, 21):
+        train(epoch)
+        test(epoch)
+        with torch.no_grad():
+            sample = torch.randn(64, 20).to(device)
+            sample = model.decode(sample).cpu()
+            save_image(sample.view(64, 1, 28, 28),
+                       'results/sample_' + str(epoch) + '.png')
