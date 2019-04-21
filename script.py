2780b1b69e022bcd405cb6d9dd8c8b7c6b1e8ada
user name <alexandre.marcil@gmail.com>
2019-04-21 03:16:18 -0400

    interpolation et sanity check

--- script.py
+++ script.py
@@ -23,8 +23,8 @@ n_critic = 5
 sample_z = torch.randn((batch_size, z_dim)).to(device)
 
 
-def visualize_sample(z_latent, model_name, epsilon = 0,  dim_eps = 5):
-    dir_recon = 'reconstruction'
+def visualize_sample(z_latent, model_name, epsilon = 0, dim_eps = 5):
+    dir_recon = 'latent_z'
     if not os.path.exists(samples_dir + '/' + model_name):
         os.makedirs(samples_dir + '/' + model_name)
 
@@ -61,15 +61,16 @@ def visualize_sample(z_latent, model_name, epsilon = 0,  dim_eps = 5):
         utils.save_images(samples[:image_frame_dim * image_frame_dim, :, :, :], [image_frame_dim, image_frame_dim],
                         samples_dir + '/' + model_name + '/' + model_name + '.png')
 
+    return z_samples
 
 def load(model_name):
     save_path = os.path.join(save_dir, dataset, model_name)
-
     model.load_state_dict(torch.load(os.path.join(save_path, model_name + '.pkl')))
 
+
 if __name__ == "__main__":
+
     model_name = 'vae'
-    
 
     if model_name == 'vae':
         model = VAE().to(device)
@@ -78,9 +79,40 @@ if __name__ == "__main__":
     
     load(model_name)
     z_latent = torch.randn((batch_size, z_dim)).to(device)
-    
-    visualize_sample(z_latent, model_name)
-    
-    epsilon = 10000
-    for dim_eps in range(z_dim):
-        visualize_sample(z_latent, model_name, epsilon, dim_eps)
+    # z_latent = torch.randn((2, 10)).to(device)
+
+    # visualize_sample(z_latent, model_name)
+    # visualize_sample(z_latent, model_name, 10000, 0)
+
+    # z_latent = torch.randn((batch_size, z_dim)).to(device)
+    # z_latent = torch.randn((2, 10)).to(device)
+
+    # visualize_sample(z_latent, model_name, 2)
+    # n1 = visualize_sample(z_latent, model_name, 1, 1)
+    # n2 = visualize_sample(n1, model_name, 1, 2)
+    # n3 = visualize_sample(n2, model_name, 1, 3)
+    # n4 = visualize_sample(n3, model_name, 1, 4)
+    # n5 = visualize_sample(n4, model_name, 1, 5)
+    # n6 = visualize_sample(n5, model_name, 1, 6)
+    #
+    # for dim_eps in range(z_dim):
+    #     tmp = visualize_sample(z_latent, model_name, epsilon, dim_eps)
+
+    def testrec(input, dim_eps):
+
+        if dim_eps < 99:
+            output = visualize_sample(input, model_name, 10, dim_eps)
+            return (testrec(output, dim_eps+1))
+        else:
+            return visualize_sample(input, model_name, 10, dim_eps)
+
+    final = testrec(z_latent, 0)
+    print(final)
+
+
+    # epsilon = 10000
+    # for dim_eps in range(z_dim):
+    #     visualize_sample(z_latent, model_name, epsilon, dim_eps)
+
+
+
