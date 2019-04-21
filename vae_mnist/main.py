76b1c2afce5f8d26e3d8a9b8a5f6c1a4c760678b
Lawrence Abdulnour <Lawrenceee94@gmail.com>
2019-04-21 11:44:49 -0400

    update changes for 2.2

--- vae_mnist/main.py
+++ vae_mnist/main.py
@@ -6,6 +6,7 @@ from torch import nn, optim
 from torch.nn import functional as F
 from VAE import VAE
 from numpy.linalg import inv
+import numpy as np
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 
@@ -88,7 +89,7 @@ def train(epoch):
           epoch, train_loss / len(train_loader.dataset)))
 
 # Testing function
-def test(epoch):
+def test(epoch, K):
     model.eval()
     test_loss = 0
 
@@ -97,33 +98,100 @@ def test(epoch):
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
             test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            if i == 0:
-                n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n],
-                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+
+            #Part to obtain the loglikelihood of x
+            z = []
+            M = data.size(dim=0)
+            D = data.size(dim=2)*data.size(dim=3)
+            L = mu.size(dim=1)
+            mu = mu.cpu().numpy()
+            logvar = logvar.cpu().numpy()
+
+            for j in range(K):
+                eps = np.random.normal(0, 1, (M, L))
+                z.append(mu + np.exp(0.5*logvar)*eps)
+
+            x = data.cpu().numpy().reshape(M,D)
+            z = np.array(z).reshape(M, -1, L)
+            loglikelihood = loglike_of_x(model, data, z)
+            logli = loglikelihood.loglike()
+            print('====> Loglikelihood of p(x): {:.4f}'.format,logli)
+            #if i == 0:
+                #n = min(data.size(0), 8)
+                #comparison = torch.cat([data[:n],
+                                      #recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
                 # save_image(comparison.cpu(), 'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+
+
 
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
 
 # Function to evaluate log-likelihood of p(x) with variational autoencoders
-def loglike(model, x, z):
+class loglike_of_x():
 
-    N = lambda x: np.exp(-x ** 2 / 2.) / ((2 * np.pi) ** 0.5)
-    n = np.size(z, axis=2)
-    K = np.size(z, axis=1)
-    exp = np.exp((x-mu).T.dot(inv(np.diag(logvar.exp())))).dot((x-mu))
-    denom = (((2 * np.pi)**(n))* np.linalg.det(logvar.exp()))**(0.5)
-    gauss_z = exp / denom
+    def __init__(self, model, x, z):
+       self.model = model
+       self.x = x
+       self.z = z
+       self.n = np.size(z, axis=2)
+       self.K = np.size(z, axis=1)
+       self.M = x.size(dim=0)
+       self.D = x.size(dim=2)*x.size(dim=3)
 
-    bce = F.binary_cross_entropy(z, np.repeat(x.view(-1, 784), K, axis=1), reduction='sum')
 
-    g_z = np.log(bce) + np.log(N(z)) - np.log(gauss_z)
+    def _forward_loop(self):
 
-    logli = g_z.max() + np.log( (np.exp(g_z - g_z.max())).sum(axis=1) ) - np.log(K)
+        recon_x, mu, logvar = self.model(self.x)
+        return recon_x, mu, logvar
 
-    return logli
+    def _standard_normal(self, z):
+
+        exp = np.exp( (-0.5)*( (z).T.dot(inv(np.diag(np.exp(np.ones(z.shape))))).dot((z))) )
+        denom = (((2 * np.pi)**(self.n))* np.linalg.det(np.diag(np.exp(np.ones(z.shape)))))**(0.5)
+
+        return exp/denom
+
+    def _normal(self, z, mu, logvar):
+
+        exp = np.exp( (-0.5)*( (z-mu).T.dot(inv(np.diag(np.exp(logvar)))).dot((z-mu))) )
+        denom = (((2 * np.pi)**(self.n))* np.linalg.det(np.diag(np.exp(logvar))))**(0.5)
+        return exp/denom
+
+    def loglike(self):
+
+        recon_x, self.mu, self.logvar = self._forward_loop()
+
+        bce = []
+        normal_standard = []
+        normal = []
+
+        # 3 values to calculate for this loop: bce, normal_standard and normal
+
+        for i in range(self.K):
+            for m in range(self.M):
+                normal_standard.append(self._standard_normal(self.z[m,i,:]))
+                normal.append(self._normal(self.z[m,i,:], self.mu[m].cpu().numpy(), self.logvar[m].cpu().numpy()))
+
+            x_tilde = self.model.decode(torch.from_numpy(self.z[:,i,:]).float().to(device))
+            bce.append(F.binary_cross_entropy(x_tilde, self.x, reduce=False, reduction='none').resize(self.M, self.D).sum(dim=1))
+
+
+        #All have to be of dimension (M, K)
+        bce = torch.stack(bce).cpu().numpy().reshape(self.M, self.K)
+        normal_standard = np.array(normal_standard).reshape(self.M, self.K)
+        normal = np.array(normal).reshape(self.M, self.K)
+
+        #has to be of dimension (M, K)
+        g_z = np.log(bce) + np.log(normal) - np.log(normal_standard)
+
+        #has to be of dimension (M,)
+        logli = g_z.max(axis=1) +\
+                np.log( (np.exp(g_z - np.repeat(np.expand_dims(g_z.max(axis=1), axis=1), self.K, axis=1))).sum(axis=1) ) -\
+                np.repeat(np.log(self.K), self.M, axis=0)
+
+        return logli
 
 
 
@@ -131,7 +199,7 @@ if __name__ == "__main__":
 
     for epoch in range(1, args.epochs + 1):
         train(epoch)
-        test(epoch)
+        test(epoch, K=200)
         # with torch.no_grad():
             # sample = torch.randn(100, 256).to(device)
             # sample = model.decode(sample).to(device)
